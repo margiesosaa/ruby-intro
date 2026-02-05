0705676ed96cdc55efc35a84aa6f91832bc43009
margiesosaa <maryori.sosa@kellogg.northwestern.edu>
2026-02-05 21:06:52 +0000

    completed complex hashes

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -14,4 +14,23 @@ puts profile
 name = profile ["name"]
 puts "Hello, #{name}"
 
+profile["age"] = 44
+
+# puts profile
+profile["name"] = "Benjamin"
+puts profile
+
 # More Complex Hashes
+profile["location"] = {"city" => "Chicago", "state" => "IL"}
+puts profile
+
+timeline = [
+{"status" => "Teaching", "occurred_at" => "8:30pm"},
+{"status" => "Reading", "occured_at" => "10:30pm"},
+{"status" => "Sleeping", "occured_at" => "12:30am"}
+]
+
+profile["timeline"] = timeline
+puts profile
+puts "Right now:"
+puts profile ["timeline"] [0]["status"]

8a3557fc306c3b8b896f49ee5574563d4db43283
margiesosaa <maryori.sosa@kellogg.northwestern.edu>
2026-02-05 21:28:27 +0000

    completed loops exercise

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -4,8 +4,26 @@
 
 # Infinite Tacos -> Finite Tacos
 # loop do
-#   puts "tacos!"
+# puts "tacos!"
 # end
 
 # Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+tacos = ["carnitas", "carne asada", "pollo", "pescado", "avocado"]
+
+index = 0
+
+loop do
+  if index == tacos.count
+    break
+  end
+  flavor = tacos[index]
+  puts "#{flavor} tacos"
+
+  index = index + 1
+end
+
+#short-hand for above:
+
+# for ___ in tacos
+# puts "#{taco} tacos"
+# end
